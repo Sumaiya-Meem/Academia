aa29c80a875dff280ec301abab33483d0100a898
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-04-02 13:42:18 +0600

    design login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,18 +1,16 @@
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
-
+import bg from "../../assets/login.avif"
 import { ContextProvider } from "../Context/AuthProvider";
 import { FcGoogle } from "react-icons/fc";
+import Swal from "sweetalert2";
 
 const Login = () => {
-  const { signInUser, signInGoogle } = useContext(ContextProvider);
+  const { signInUser} = useContext(ContextProvider);
 
-  const navigate = useNavigate();
-  const location = useLocation();
 
-  const from = location.state?.from?.pathname || "/";
 
   const {
     register,
@@ -33,7 +31,7 @@ const Login = () => {
               popup: 'animate__animated animate__fadeOutUp'
           }
       });
-      navigate(from, { replace: true });
+  
       })
       .catch((error) => {
         console.log(error.message);
@@ -41,21 +39,24 @@ const Login = () => {
       });
   };
 
- 
+ const handleGoogle =() =>{
+
+ }
   const backgroundImageStyle = {
-    backgroundImage: `url(${bg})`,
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${bg})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
     height: "100vh",
   };
 
   return (
-    <div className="flex justify-between" style={backgroundImageStyle}>
-      <div className="flex shadow-lg mt-28 items-center" >
+    <div style={backgroundImageStyle} className="w-full bg-gradient-to-r from-[#00000033] to-[#00000033] mt-0">
+        <div className="flex items-center justify-center w-[100%] md:w-[60%] lg:w-[40%] mx-auto " >
+      <div className="mt-28 text-white p-2 w-full bg-gray-400 shadow-lg rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-600">
  
-      <div className="">
+   
         <form
-          className="w-full  p-8 "
+          className=""
           onSubmit={handleSubmit(onSubmit)}
         >
           <h2 className="text-2xl font-bold mb-4">Login</h2>
@@ -63,7 +64,7 @@ const Login = () => {
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-white text-sm font-bold mb-2"
             >
               Email
             </label>
@@ -73,8 +74,8 @@ const Login = () => {
               {...register("email", {
                 required: "Email is required",
               })}
-              className={`w-full p-2 border block rounded ${
-                errors.email ? "border-red-500" : "border-gray-300"
+              className={`w-full p-2 border block rounded bg-transparent ${
+                errors.email ? "border-red-500" : "border-gray-500"
               }`}
             />
             {errors.email && (
@@ -87,7 +88,7 @@ const Login = () => {
           <div className="mb-4">
             <label
               htmlFor="password"
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-white text-sm font-bold mb-2"
             >
               Password
             </label>
@@ -95,8 +96,8 @@ const Login = () => {
               type="password"
               id="password"
               {...register("password", { required: "Password is required" })}
-              className={`w-full p-2 block border rounded ${
-                errors.password ? "border-red-500" : "border-gray-300"
+              className={`w-full p-2 block border rounded  bg-transparent  ${
+                errors.password ? "border-red-500" : "border-gray-500"
               }`}
             />
             {errors.password && (
@@ -109,7 +110,7 @@ const Login = () => {
           <div className="flex flex-col items-center gap-2 justify-between">
             <button
               type="submit"
-              className=" px-4 py-2 bg-orange-200 w-full rounded-md font-bold  focus:outline-none focus:shadow-outline-blue"
+              className=" px-4 py-2 bg-blue-900 w-full rounded-md font-bold  focus:outline-none focus:shadow-outline-blue"
             >
               Login
             </button>
@@ -118,21 +119,22 @@ const Login = () => {
             </div>
             <div
               onClick={handleGoogle}
-              className="w-full text-center border border-blue-600  px-4 py-2 rounded-full flex items-center justify-center gap-1 text-xl"
+              className="w-full text-center border bg-blue-900  px-4 py-2 rounded-full flex items-center justify-center gap-1 text-xl"
             >
                 <p><FcGoogle></FcGoogle></p>
               <p>Google</p>
             </div>
           </div>
           <h1 className="mt-5">
-            Are You New Here?{" "}
-            <Link className="text-blue-700 underline" to="/registration">
-              Registration
+            Don't have any account ? 
+            <Link className="text-blue-400 underline ml-1" to="/registration">
+               Registration
             </Link>
           </h1>
         </form>
+    
       </div>
-      </div>
+    </div>
     </div>
   );
 };
