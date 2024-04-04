75aff8ddde394bcf94ce2ef72299623939253c87
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-04-04 20:50:54 +0600

    fully complete the category design section

--- src/Pages/HomePage/Category/Category.jsx
+++ src/Pages/HomePage/Category/Category.jsx
@@ -6,22 +6,43 @@ const Category = () => {
   if (!category) {
     return <div>Loading...</div>;
   }
+
+  const backgroundImageStyle = {
+    backgroundImage: `linear-gradient(rgba(5,10,35,0.75), rgba(5,10,35,0.75)), url('https://i.ibb.co/6rDbq6F/category.jpg)`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: '200px',
+  };
+
+
   return (
     <div className="">
-      <div>
-        <h1 className="text-2xl lg:text-[40px]  mb-20 font-bold font-[Poppins] text-[#22323d]">
-          Creative Categories <br></br> For Students{" "}
-        </h1>
+        
+      {/* cover section */}
+      <div style={backgroundImageStyle} className="relative mb-20">
+      <div className="h-[200px] text-gray-300 font-serif flex flex-col items-center justify-center mt-10 bg-slate-500 bg-opacity-20">
+        <h1 className="text-3xl uppercase"> Creative Categories</h1>
+        <h1 className="text-3xl uppercase mt-2"> For Students</h1>
+        {/* <h4 className='capitalize '> {para}</h4> */}
       </div>
+      </div>
+     
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-3 ">
         {category.map((data, index) => (
           <div key={index} className="card mb-6  p-4 ">
-             <div className="icon"></div>
-            <div className="content">
-              <h1 className="">{data.title}</h1>
-              <p className="p-2">{data.description}</p>
+            <div className="icon">
+              <div className="imgBox">
+                <img src={data.photo} alt="" className="" />
+              </div>
             </div>
-           
+            <div className="content">
+              <h1 className="mt-16 text-center text-2xl font-semibold font-serif">
+                {data.title}
+              </h1>
+              <p className="p-2 text-justify text-gray-700 text-sm">
+                {data.description}
+              </p>
+            </div>
           </div>
         ))}
       </div>
