216d1b6cd678844c1906967dd60186c4abfde6d3
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-04-04 20:27:04 +0600

    load category data with image

--- src/Pages/DashboardPage/AdminPage/CreativeCategory/CreativeCategory.jsx
+++ src/Pages/DashboardPage/AdminPage/CreativeCategory/CreativeCategory.jsx
@@ -2,72 +2,109 @@ import { useForm } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
 
+const IMG_HOASTING_KEY = import.meta.env.VITE_IMAGE_UPLOAD_API;
+const img_hosting_api = `https://api.imgbb.com/1/upload?key=${IMG_HOASTING_KEY}`
 
 const CreativeCategory = () => {
-    const { handleSubmit, register,reset} = useForm();
-    const axiosPublic = useAxiosPublic();
+  const { handleSubmit, register, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
 
-    const onSubmit =async (data) => {
-        const category = {
-            title:data.title,
-            description: data.description
+  const onSubmit = async (data) => {
+    const imgFile = { image: data.photo[0] }
+    console.log(imgFile)
+    const res = await axiosPublic.post(img_hosting_api, imgFile, {
+        headers: {
+            'Content-Type': "multipart/form-data"
         }
-        console.log(category)
-        axiosPublic.post('/category', category)
-        .then(res=> {
-            console.log(res.data)
-               if(res.data.acknowledged){
-                
-                       toast.success('create category successfully')
-                       
-               }
-        })
-        reset();
-    }
-    return (
-        <div>
-            <div className="max-w-md mx-auto mt-8 p-8 border-2 border-blue-900 rounded shadow-md shadow-blue-950">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <h2 className="text-3xl font-semibold mb-4">Make Creative Category</h2>
+    })
+    console.log(res);
+    const img_url = res?.data?.data?.display_url;
+    console.log(img_url)
+    if(img_url){
+    const category = {
+      title: data.title,
+      description: data.description,
+      photo: img_url 
+    };
+    console.log(category);
+    axiosPublic.post("/category", category).then((res) => {
+      console.log(res.data);
+      if (res.data.acknowledged) {
+        toast.success("create category successfully");
+      }
+    });
+    reset();
+}
+  };
+  return (
+    <div>
+      <div className="max-w-md mx-auto mt-8 p-8 border-2 border-blue-900 rounded shadow-md shadow-blue-950">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="text-3xl font-semibold mb-4">
+            Make Creative Category
+          </h2>
 
-                    {/* Title Field */}
-                    <div className="mb-4">
-                        <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-2">
-                            Title
-                        </label>
-                        <input
-                            type="text"
-                            id="title"
-                            {...register('title', { required: 'Title is required' })}
-                            className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
-                        />
-                    </div>
+          {/* Title Field */}
+          <div className="mb-4">
+            <label
+              htmlFor="title"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              {...register("title", { required: "Title is required" })}
+              className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
+            />
+          </div>
 
-                    {/* Description Field */}
-                    <div className="mb-4">
-                        <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2">
-                            Description
-                        </label>
-                        <textarea
-                            id="description"
-                            {...register('description', { required: 'Description is required' })}
-                            className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
-                        />
-                    </div>
+          {/* Description Field */}
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              {...register("description", {
+                required: "Description is required",
+              })}
+              className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
+            />
+          </div>
 
-                   
+          {/* Image */}
 
-                    <button
-                        type="submit"
-                        className="bg-blue-900 text-white py-2 px-4 rounded hover:bg-blue-900 focus:outline-none focus:ring focus:border-blue-700"
-                    >
-                        Submit
-                    </button>
-                </form>
-            </div>
-            <Toaster></Toaster>
-        </div>
-    );
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Photo
+            </label>
+            <input
+              type="file"
+              id="photo"
+              {...register("photo", { required: "Photo is required" })}
+              className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-blue-900 text-white py-2 px-4 rounded hover:bg-blue-900 focus:outline-none focus:ring focus:border-blue-700"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <Toaster></Toaster>
+    </div>
+  );
 };
 
 export default CreativeCategory;
--- src/Pages/HomePage/Category/Category.jsx
+++ src/Pages/HomePage/Category/Category.jsx
@@ -1,36 +1,32 @@
 import useCategory from "../../../Hooks/useCategory";
 
-import './Category.css'
+import "./Category.css";
 const Category = () => {
-    const {category } = useCategory();
-    if (!category ) {
-        return <div>Loading...</div>;
-    }
-    return (
-        <div>
-            
-            <div>
-                    <h1 className="text-2xl lg:text-[40px]  mb-10 font-bold font-[Poppins] text-[#22323d]">Creative Categories <br></br> For Students </h1>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-3">
-                    {category.map((data, index) => (
-                         <div key={index} className="card mb-6  p-4 " >
-                            <div className="box">
-                                <div className="icon">
-                                    <div className="iconbox"></div>
-
-                                </div>
-                                <div className="content">
-                                <h1 className="">{data.title}</h1>
-                            <p className="p-2">{data.description}</p>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    ))}
-                </div>
-        </div>
-    );
+  const { category } = useCategory();
+  if (!category) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="">
+      <div>
+        <h1 className="text-2xl lg:text-[40px]  mb-20 font-bold font-[Poppins] text-[#22323d]">
+          Creative Categories <br></br> For Students{" "}
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-3 ">
+        {category.map((data, index) => (
+          <div key={index} className="card mb-6  p-4 ">
+             <div className="icon"></div>
+            <div className="content">
+              <h1 className="">{data.title}</h1>
+              <p className="p-2">{data.description}</p>
+            </div>
+           
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
